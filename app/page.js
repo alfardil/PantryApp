f0d7d73ccbbef954c841acf00e03067a01a5828c
alfardil <fardilfayed@gmail.com>
2024-08-01 15:00:51 -0400

    added buttons / textfield to customize amount added or decreased by

--- app/page.js
+++ app/page.js
@@ -4,11 +4,13 @@ import {useState, useEffect} from "react"
 import {firestore} from "@/firebase"
 import {Box, Modal, Typography, Stack, TextField, Button} from "@mui/material"
 import {collection, deleteDoc, doc, getDocs, query, getDoc, setDoc} from "firebase/firestore"
+import './globals.css';
 
 export default function Home() {
   const [inventory, setInventory] = useState([])
   const [open, setOpen] = useState(false)
   const [itemName, setItemName] = useState("")
+  const [quantityInput, setQuantityInput] = useState(1);
 
   const updateInventory = async () => {
     const snapshot = query(collection(firestore, "inventory"))
@@ -29,10 +31,10 @@ export default function Home() {
 
     if (docSnap.exists()){
       const {quantity} = docSnap.data()
-      await setDoc(docRef, {quantity: quantity + 1})
+      await setDoc(docRef, {quantity: quantity + quantityInput})
     }
     else {
-      await setDoc(docRef, {quantity: 1})
+      await setDoc(docRef, {quantity: quantityInput})
     }
     await updateInventory()
   }
@@ -48,7 +50,7 @@ export default function Home() {
         await deleteDoc(docRef)
       }
       else {
-        await setDoc(docRef, {quantity: quantity - 1})
+        await setDoc(docRef, {quantity: quantity - quantityInput})
       }
     }
 
@@ -71,6 +73,7 @@ export default function Home() {
     justifyContent="center"
     alignItems="center"
     gap={2}
+    bgcolor="#edd96c"
     >
       <Modal open={open} onClose={handleClose}>
         <Box 
@@ -78,7 +81,7 @@ export default function Home() {
         top="50%" 
         left="50%"
         width={400}
-        bgcolor="#FFFFFF"
+        bgcolor="#edd96c"
         border="2px solid #000"
         boxShadow={24}
         px={4}
@@ -90,15 +93,22 @@ export default function Home() {
           transform: 'translate(-50%,-50%)',
         }}
         > 
-          <Typography variant="h6"> Add Item </Typography>
-          <Stack width="100%" direction="row" spacing={2}>
+          <Typography variant="h6" className="space-mono-regular"> Add Item </Typography>
+          <Stack 
+          width="100%" 
+          direction="row" 
+          justifyContent="space-between"
+          spacing={2}
+          >
+
             <TextField
             variant="outlined"
             fullWidthvalue={itemName}
             onChange={(e) => {
-              setItemName(e.target.value)
+              setItemName(e.target.value.trim().toLowerCase())
             }}
             />
+
             <Button
             variant="outlined"
             onClick={() => {
@@ -106,31 +116,50 @@ export default function Home() {
               setItemName('')
               handleClose()
             }}
+            className="space-mono-regular"
             >
               Add
             </Button>
+
           </Stack>
+
         </Box>
+
       </Modal>
-      <Button variant="contained" onClick={()=>{
+
+      <Button 
+      variant="contained"
+       onClick={()=>{
         handleOpen()
-      }}>
+      }}
+      className="space-mono-regular"
+      >
         Add New Item
       </Button>
+
       <Box border="1px solid #333">
         <Box
         width="800px"
         height="100px"
-        bgcolor="#ADD8E6"
+        bgcolor="#edd96c"
         display="flex"
         alignItems="center"
         justifyContent="center"
         >
-          <Typography variant="h2" color="#333">
+
+          <Typography variant="h2" color="#333" className="space-mono-regular">
             Inventory Items
           </Typography>
+
         </Box>
-      <Stack width="800px" height="300px" spacing={2} overflow="auto">
+
+      <Stack
+      width="800px" 
+      height="300px" 
+      spacing={2} 
+      overflow="auto"
+      padding={3}
+      >
         {
           inventory.map(({name, quantity}) => (
             <Box 
@@ -140,13 +169,15 @@ export default function Home() {
             display="flex" 
             alignItems="center" 
             justifyContent="space-between" 
-            bgcolor="#f0f0f0" 
-            padding={5}>
+            bgcolor="#edd96c" 
+            padding={3}
+            >
 
               <Typography 
               variant="h3"
               color="#333"
               textAlign="center"
+              className="space-mono-regular"
               >
                 {name.charAt(0).toUpperCase() + name.slice(1)}
               </Typography>
@@ -155,18 +186,63 @@ export default function Home() {
               variant="h3"
               color="#333"
               textAlign="center"
+              className="space-mono-regular"
               >
                 {quantity}
               </Typography>
+
+            <Stack
+            orientation="vertical"
+            spacing={1}
+            >
+              
+              <TextField
+              id="standard-number"
+              label="Enter number:"
+              type="number"
+              InputLabelProps={{
+                shrink: true,
+                classes: {
+                  root: "input-label",
+                }
+              }}
+              InputProps={{
+                classes: {
+                  input: "space-mono-regular"
+                },
+              }}
+              variant="standard"
+              size="small"
+              style={{ width: '200px' }}
+              value={quantityInput}
+              onChange={(e) => setQuantityInput(Number(e.target.value))}
+              />
+
+              <Button
+              variant="contained"
+              onClick={() => {
+                addItem(name);
+              }}
+              className="space-mono-regular"
+              color="success"
+              >
+                Add
+              </Button>
 
               <Button 
               variant="contained"
               onClick={() => {
                 removeItem(name);
               }}
+              className="space-mono-regular"
+              color="error"
               >
                 Remove
               </Button>
+
+              
+
+            </Stack>
 
 
             </Box>
